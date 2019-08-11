2739c27f143ace21d6b982fc6f6db74923812a0f
mengheng <menghengxue@gmail.com>
2019-08-11 14:52:06 -0400

    update etl

--- etl.py
+++ etl.py
@@ -39,14 +39,14 @@ def process_song_data(spark, input_data, output_data):
 
     # extract columns to create songs table
     songs_table = df['song_id', 'title', 'artist_id', 'year', 'duration']
-    songs_table = songs_table.dropDuplicates()
+    songs_table = songs_table.dropDuplicates(['song_id'])
     
     # write songs table to parquet files partitioned by year and artist
     songs_table.write.partitionBy('year', 'artist_id').parquet(os.path.join(output_data, 'songs.parquet'), 'overwrite')
 
     # extract columns to create artists table
     artists_table = df['artist_id', 'artist_name', 'artist_location', 'artist_latitude', 'artist_longitude']
-    artists_table = artists_table.dropDuplicates()
+    artists_table = artists_table.dropDuplicates(['artist_id'])
     
     # write artists table to parquet files
     artists_table.write.parquet(os.path.join(output_data, 'artists.parquet'), 'overwrite')
@@ -74,7 +74,7 @@ def process_log_data(spark, input_data, output_data):
 
     # extract columns for users table    
     users_table = df['userId', 'firstName', 'lastName', 'gender', 'level']
-    users_table = users_table.dropDuplicates()
+    users_table = users_table.dropDuplicates(['userId'])
     
     # write users table to parquet files
     users_table.write.parquet(os.path.join(output_data, 'users.parquet'), 'overwrite')
@@ -97,7 +97,7 @@ def process_log_data(spark, input_data, output_data):
         month('datetime').alias('month'),
         year('datetime').alias('year') 
    )
-    time_table = time_table.dropDuplicates()
+    time_table = time_table.dropDuplicates(['start_time'])
     
     # write time table to parquet files partitioned by year and month
     time_table.write.partitionBy('year', 'month').parquet(os.path.join(output_data, 'time.parquet'), 'overwrite')
@@ -109,7 +109,20 @@ def process_log_data(spark, input_data, output_data):
 
     # extract columns from joined song and log datasets to create songplays table 
     df = df.join(song_df, song_df.title == df.song)
-    songplays_table = df['ts', 'userId', 'level', 'song_id', 'artist_id', 'sessionId', 'location', 'userAgent']
+    
+    songplays_table = df.select(
+        col('ts').alias('ts'),
+        col('userId').alias('user_id'),
+        col('level').alias('level'),
+        col('song_id').alias('song_id'),
+        col('artist_id').alias('artist_id'),
+        col('ssessionId').alias('session_id'),
+        col('location').alias('location'),
+        col('userAgent').alias('user_agent'),
+        col('year').alias('year'),
+        month('datetime').alias('month')
+    )
+    
     songplays_table = songplays_table.selectExpr("ts as start_time")
     songplays_table.select(monotonically_increasing_id().alias('songplay_id')).collect()
     
